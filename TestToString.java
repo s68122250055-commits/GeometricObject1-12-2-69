45a3b5b6e6f226f09270584c897e34245b04db7c
s68122250055-commits <s68122250055@ssru.ac.th>
2026-02-12 10:21:11 +0700

    Update TestToString.java

--- TestToString.java
+++ TestToString.java
@@ -1,7 +1,15 @@
 public class TestToString {
+    private String name;
+    public TestToString(){}
+    public TestToString(String name){
+        this.name = name;
+    }
+
     public static void main(String[] args) {
-        TestToString t1 = new TestToString();
+        TestToString t1 = new TestToString("John");
         System.out.println(t1.toString());
     }
-    
+      public String toString() {
+        return "created on"+name ;
+  }
 }
